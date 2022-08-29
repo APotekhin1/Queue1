1be83c8b19f5a38b8f0182a368011c722938b652
APotekhin1 <alex01210@gmail.com>
2022-08-29 16:07:28 +0300

    List.of - это мы не проходили )))

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    protected String name;
+    protected String surName;
+    protected int tickets;
+    public Person (String name, String surName, int tickets) {
+        this.name = name;
+        this.surName = surName;
+        this.tickets = tickets;
+    }
+
+}
